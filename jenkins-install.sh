c1087cffacca815f2e9e7a1d3f5cfbf573a62c9f
laurametchum <metchuml@gmail.com>
2024-02-09 20:47:53 -0600

    jenkins installation

---
+++ jenkins-install.sh
@@ -0,0 +1,35 @@
+sudo yum update -y
+
+sudo yum -y wget 
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum upgrade -y
+
+## install And Enable Docker
+sudo yum install docker -y
+sudo service docker start 
+sudo systemctl enable docker.service
+
+sudo chmod 777  /var/run/docker.sock
+
+
+## install Git
+sudo yum install git -y
+yum install unzip -y
+
+## Install Java 11:
+#sudo amazon-linux-extras install java-openjdk11 -y
+
+sudo yum install java-11* -y
+## Install Jenkins then Enable the Jenkins service to start at boot :
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+
+## Start Jenkins as a service:
+sudo systemctl start jenkins
+
+## Display Initial Jenkins Password
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword  
